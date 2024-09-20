e0af4c67af13849b74123a31b0fc6c814926eae4
livecodebase <livecodebase@gmail.com>
2024-09-20 16:46:52 +0530

    Fixed scroll position issue on movide detail & minor CSS uplifts

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,14 @@ const router = createRouter({
       name: 'NotFound',
       component: NotFoundView
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
